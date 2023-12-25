aeb43dfbbbe5e1434c97817aeb5fde670944c002
Magistrat <nisraton@gmail.com>
2023-12-26 01:02:10 +0300

    add doc-string, inputs, close programm

--- lab/Program.cs
+++ lab/Program.cs
@@ -4,22 +4,37 @@ using System.Runtime.InteropServices;
 
 internal class Program {
 
-    // определение библиотеки DLL, содержащей фунецию SM
+    // Определение библиотеки DLL, содержащей функцию SendMessage
     [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Auto)]
-    // Инициализация функции SM (дескриптор окна, тип сообщения, дополнительная информация о сообщении, доп информация)
     public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
-    [DllImport("user32.dll")]
 
-    // блокировка окна запущенного приложения
+    // Определение функции блокировки окна
+    [DllImport("user32.dll")]
     static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+    // Определение функции поиска окна
     [DllImport("user32.dll", SetLastError = true)]
     static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
+    // Определение сообщения для закрытия окна
+    const int WM_CLOSE = 0x0010;
+
+    // Определение названия окна
+    const string WINDOW_FOR = "Maps";
 
     private static int Main(string[] args)
     {
-        IntPtr hWnd = FindWindow(null, "Calendar");
+        Console.WriteLine("For block " + WINDOW_FOR + " press to Enter...");
+        Console.ReadLine();
+
+        IntPtr hWnd = FindWindow(null, WINDOW_FOR);
+
+        Console.WriteLine("Blocking a program...");
         EnableWindow(hWnd, false);
+
+        Console.WriteLine("For close " + WINDOW_FOR + " press to Enter...");
+        Console.ReadLine();
+        SendMessage(hWnd, WM_CLOSE, 0, 0);
         return 0;
     }
 }
